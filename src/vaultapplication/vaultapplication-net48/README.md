fd2c7e93699eb50fece3f559f4044f4334b03acd
Victor Vogelpoel <victor@victorvogelpoel.nl>
2021-10-02 17:03:42 +0200

    feat: add vaultapplication-net48
    This sample Vault Application has target .NET Framework 4.8, instead of the default 4.5, and demonstrates that it can run in the vault.
    .NET framework 4.5 is no longer supported by Microsoft and 4.8 opens the vault application to use more recent code packages.

---
+++ src/vaultapplication/vaultapplication-net48/README.md
@@ -0,0 +1,5 @@
+# M-Files sample vault application vaultapplication-net48
+
+This sample Vault Application has target .NET Framework 4.8, instead of the default 4.5, and demonstrates that it can run in the vault.
+.NET framework 4.5 is no longer supported by Microsoft and 4.8 opens the vault application to use more recent code packages.
+
---
+++ src/vaultapplication/vaultapplication-net48/VaultApplication.cs
@@ -0,0 +1,42 @@
+// VaultApplication.cs
+// 2-10-2021
+// Copyright 2021 Dramatic Development - Victor Vogelpoel
+// If this works, it was written by Victor Vogelpoel (victor@victorvogelpoel.nl).
+// If it doesn't, I don't know who wrote it.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+using System;
+using MFiles.VAF.Common;
+using MFiles.VAF.Core;
+using MFilesAPI;
+
+namespace VaultapplicationNet48
+{
+    /// <summary>
+    /// The entry point for this Vault Application Framework application.
+    /// </summary>
+    /// <remarks>Examples and further information available on the developer portal: http://developer.m-files.com/. </remarks>
+    public class VaultApplication : ConfigurableVaultApplicationBase<Configuration>
+    {
+        /// <summary>
+        /// Sample Event that fired upon checkin of a Builtin Document object type.
+        /// </summary>
+        /// <param name="env"></param>
+        [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCheckInChangesFinalize, ObjectType = (int)MFBuiltInObjectType.MFBuiltInObjectTypeDocument)]
+        public void BeforeCheckInChangesFinalizeUpdateLogDemo(EventHandlerEnvironment env)
+        {
+            SysUtils.ReportInfoToEventLog($"User {env.CurrentUserID} checked in document object {env.ObjVer.ID}");
+        }
+    }
+}
